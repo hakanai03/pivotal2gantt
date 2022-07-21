37171b213046df8f18e0c999961a840c90601a93
Shogo Ishimaru <hakanai.blog@gmail.com>
2022-07-21 23:01:19 +0900

    update

--- src/pages/pivotal2gantt/components/TaskDetailTable.tsx
+++ src/pages/pivotal2gantt/components/TaskDetailTable.tsx
@@ -1,19 +1,36 @@
 import {Ticket} from "@/types/pivotal/Ticket"
-import {Table} from "antd"
+import {Divider, Table, Typography} from "antd"
 import {ColumnsType, ColumnType} from "antd/lib/table"
 import {useContext} from "react"
 import {Pivotal2GanttContext} from "../Pivotal2GanttProvider"
 
+const columns: ColumnsType<Ticket> = [
+  {
+    title: "プロジェクト大項目",
+    dataIndex: "labels",
+    key: "labels",
+  },
+  {
+    title: "概要",
+    dataIndex: "description",
+    key: "description",
+    render: (value: string) => <div style={{whiteSpace: "pre-wrap"}}>{value.replace(/\t/g,"")}</div>,
+  },
+]
+
 export const TaskDetailTable = () => {
   const {state, setState} = useContext(Pivotal2GanttContext)
-
-  const columns: ColumnsType<Ticket> = state.tickets.length > 0 ? Object.keys(state.tickets[0]).map(key => ({key, title: key, dataIndex: key})) : []
+  const data = state.tickets.filter(ticket => ticket.type === "release").filter(ticket => ticket.labels !== "")
+  console.log(data)
 
   return (
-    <Table<Ticket>
-      dataSource={state.tickets}
-      columns={columns}
-      pagination={{pageSize: 200}}
-    />
+    <div>
+      <Typography.Title level={3}>Releases</Typography.Title>
+      <Table<Ticket>
+        dataSource={data}
+        columns={columns}
+        pagination={{pageSize: 200}}
+      />
+    </div>
   )
 }
